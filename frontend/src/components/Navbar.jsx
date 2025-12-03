ab5889b2c04ca36438aa8dc6f7588196d9806732
jie-ui <aliceyangkk@gmail.com>
2025-12-03 10:45:49 -0500

    update navbar add register button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,9 +45,15 @@ export default function Navbar() {
 
         {/* Login / Logout */}
         {!user ? (
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
+          <>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+
+            <Button color="inherit" component={Link} to="/register">
+              Register
+            </Button>
+          </>
         ) : (
           <Button color="inherit" onClick={handleLogout}>
             Logout
